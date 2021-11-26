d7c9a1ca474a9afb7368385bac5915b684e92be4
michaelOch <ochuenwikechukwudi@hotmail.com>
2021-11-26 05:04:15 +0200

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,12 @@
+// const navCaption = document.querySelector('.nav-caption');
+
+// //  Add Event Listeners
+// document.addEventListener('mouseover', handleEvents);
+
+// function handleEvents(e) {
+
+//     if(e.target.classList.contains('icons-container')) {
+//         e.target.childNodes[1].childNodes[0].style.display = "inline-block"
+//         console.log(e.target.childNodes[1].childNodes[0])
+//     }
+// }
